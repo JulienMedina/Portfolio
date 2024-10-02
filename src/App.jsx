ee8bfbc8a8ae73a6b43c6a335713e02659657e20
JulienMedina <julien.medina16@gmail.com>
2024-10-02 20:19:48 +0200

    feat: header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 //import About from "./components/About";
 //import Projects from "./components/Projects";
 //import Skills from "./components/Skills";
@@ -6,8 +7,10 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
+    <div className="flex min-h-screen flex-col bg-grayDark-1">
       <Header />
+      <main className="grow"></main>
+      <Footer />
       {/* <About /> */}
       {/* <Projects /> */}
       {/* <Skills /> */}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,28 +2,60 @@
 
 const Header = () => {
   return (
-    <header className="bg-base-100 shadow-md">
-      <div className="container mx-auto p-4">
-        <h1 className="text-3xl font-bold">Julien Medina</h1>
-        <nav>
+    <header className="w-full bg-auto font-sans shadow-md" role="banner">
+      <div className="container mx-auto flex items-center justify-between p-4">
+        {/* Initiales à gauche */}
+        <div
+          className="text-2xl font-bold text-grayDark-12"
+          aria-label="Initiales JM"
+        >
+          JM
+        </div>
+        {/* Navigation à droite */}
+        <nav aria-label="Main Navigation">
           <ul className="flex space-x-4">
             <li>
-              <a href="#about" className="text-blue-500">
+              <a
+                href="#home"
+                className="text-grayDark-12 transition-colors duration-300 hover:text-grayDark-11"
+                aria-label="Accueil"
+              >
+                Accueil
+              </a>
+            </li>
+            <li>
+              <a
+                href="#about"
+                className="text-grayDark-12 transition-colors duration-300 hover:text-grayDark-11"
+                aria-label="À propos"
+              >
                 À propos
               </a>
             </li>
             <li>
-              <a href="#projects" className="text-blue-500">
+              <a
+                href="#projects"
+                className="text-grayDark-12 transition-colors duration-300 hover:text-grayDark-11"
+                aria-label="Projets"
+              >
                 Projets
               </a>
             </li>
             <li>
-              <a href="#skills" className="text-blue-500">
+              <a
+                href="#skills"
+                className="text-grayDark-12 transition-colors duration-300 hover:text-grayDark-11"
+                aria-label="Compétences"
+              >
                 Compétences
               </a>
             </li>
             <li>
-              <a href="#contact" className="text-blue-500">
+              <a
+                href="#contact"
+                className="text-grayDark-12 transition-colors duration-300 hover:text-grayDark-11"
+                aria-label="Contact"
+              >
                 Contact
               </a>
             </li>
